13365f93a480c5ae2a65da4047a5e5acd8d0f6b9
Dhana Bhuma <dhana.bhuma13@gmail.com>
2023-08-20 13:06:31 -0500

    Comments added

--- hello.js
+++ hello.js
@@ -3,17 +3,27 @@ Welcome, new coder.
 Enter your code on line 7 to print 'Hello, World!' 
 to the console (the panel on the right)
 */
+console.log("Hello, World!")
+// 1. Change the message that is printed.
+//2. Figure out what the parentheses do. Will the code work without them?
+//3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+//4. Remove the semi-colon, ;.
 
 console.log ("Hello Dhana! welcome to LaunchCode")
+//5. Print a number. (Bonus: Print two numbers added together).
 a = 6
 console.log(a)
+
 c =2 + 3;
 console.log(c)
+//6. Print multiple messages one after the other.
 console.log("JavaScript")
 console.log("FullStack")
+//7. Print two messages on the same line.
 message1 = "My favorite car is:"
 message2 = "Toyota RAV4"
 console.log(message1 , message2)
+//8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
 message3 = "printing string in \"quotation marks\" using java script"
 console.log(message3)
 
